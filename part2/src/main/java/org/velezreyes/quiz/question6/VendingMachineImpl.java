d1c94301cf51d72f64abc22fd3ab49ea604068e2
Juan Fernando Martinez <jm1811324@gmail.com>
2023-11-04 23:14:12 -0500

    part 2 done

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,61 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import java.util.HashMap;
+
+public class VendingMachineImpl implements VendingMachine {
+
+
+  private final static HashMap<String, Double> prices = new HashMap<>();
+  private static  VendingMachine instance;
+
+
+
+  private double money;
+
+  private VendingMachineImpl(){
+      this.money = 0;
+      prices.put("ScottCola", 0.75);
+      prices.put("KarenTea", 1.0);
+  }
 
   public static VendingMachine getInstance() {
     // Fix me!
-    return null;
+    if (instance == null){
+      instance = new VendingMachineImpl();
+    }
+    return instance;
+  }
+
+  @Override
+  public void insertQuarter() {
+      money += 0.25;
+  }
+
+
+  private Drink createDrink(String name){
+    return new Drink() {
+      @Override
+      public String getName() {
+        return name;
+      }
+
+      @Override
+      public boolean isFizzy() {
+        return name.contains("Cola");
+      }
+    };
+  }
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    if (prices.containsKey(name)){
+      if (money >= prices.get(name)){
+        money-= prices.get(name);
+        return createDrink(name);
+      }else{
+        throw new NotEnoughMoneyException();
+      }
+    }else {
+      throw new UnknownDrinkException();
+    }
   }
 }
